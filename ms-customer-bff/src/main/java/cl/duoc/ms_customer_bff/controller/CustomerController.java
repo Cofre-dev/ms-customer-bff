0adf78c49d736305e6d2f2e73f7aa722a620da03
Matias Cofré <rojascofrem@gmail.com>
2025-05-15 18:26:14 -0400

    Add front
    Añadiendo el front de la web

--- ms-customer-bff/src/main/java/cl/duoc/ms_customer_bff/controller/CustomerController.java
+++ ms-customer-bff/src/main/java/cl/duoc/ms_customer_bff/controller/CustomerController.java
@@ -7,10 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import cl.duoc.ms_customer_bff.model.dto.CustomerDTO;
 import cl.duoc.ms_customer_bff.service.CustomerService;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @RestController
+@CrossOrigin("*")
 public class CustomerController {
 
     @Autowired
